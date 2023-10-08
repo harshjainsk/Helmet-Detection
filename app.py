bf8efbfee6213f535a1be6bfa89f7fb48bae95a7
harshjainsk <harshjainsk531@gmail.com>
2023-10-08 22:36:44 +0530

    added result visualizations

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ import cv2
 model_path = os.path.join("E:/wobot-git/runs/train/exp/weights/last.pt")
 model = torch.hub.load('ultralytics/yolov5', 'custom', path = model_path, force_reload = True)
 
-import os
-image_path = os.path.join('E:/wobot-git/testing_images/test_image_3.jpeg')
+image_path = os.path.join('E:/wobot-git/testing_images/test_image_4.png')
+
+img = cv2.imread("testing_images/test_image_4.png")
+img = cv2.resize(img, (640,640))
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow("img", img)
 
 # %matplotlib inline
 
@@ -18,5 +22,6 @@ results = model(image_path)
 results.print()
 
 img = np.squeeze(results.render())
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.imshow("img", img)
 cv2.waitKey(0)
